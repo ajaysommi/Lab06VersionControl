bde2c5f8ec419c1ddffa96aef4e3d6af45d18fb0
Gabriel Frank <gfrank1@ufl.edu>
2023-10-30 19:48:11 -0400

    I added the decode function and the quit for the loop.

--- version_control.py
+++ version_control.py
@@ -3,6 +3,18 @@ def encode(raw_password):
     for i in range(len(raw_password_list)):
         encoded_password += str(int(raw_password_list[i]) + 3)
     return encoded_password
+
+
+def decode(password):
+    decode_str = ""
+    int_list = []
+    for i in password:
+        int_list += i
+    for item in int_list:
+        a = int(item) - 3
+        decode_str += str(a)
+    return decode_str
+
 def main():
     stored_password = ''
     loopActive = True
@@ -16,12 +28,14 @@ def main():
         user_input = int(input("Please enter an option: "))
         if user_input == 1:
             raw_to_encode = int(input("Please enter your passcode to encode: "))
-            print(encode(raw_to_encode))
             stored_password += encode(raw_to_encode)
             print("Your password has been encoded and stored!")
-
+            print()
         elif user_input == 2:
-            print(f"The encoded password is {stored_password}, and the original password is ")
+            print(f"The encoded password is {stored_password}, and the original password is {decode(stored_password)}.")
+        elif user_input == 3:
+            loopActive = False
+
 
 if __name__ == '__main__':
     main()
